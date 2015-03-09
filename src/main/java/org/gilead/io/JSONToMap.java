ac93696434a2eaba3b93619f404ba236f9d4307e
Martin Rose <martinrose@google.com>
2015-03-09 16:16:30 -0500

    transient work

---
+++ src/main/java/org/gilead/io/JSONToMap.java
@@ -0,0 +1,13 @@
+package org.gilead.io;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by martinrose on 3/9/15.
+ */
+public class JSONToMap {
+  final Logger log = LoggerFactory.getLogger(JSONToMap.class);
+
+
+}
